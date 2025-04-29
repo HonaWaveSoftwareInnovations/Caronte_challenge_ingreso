229ab0aa1912f258bfee35f961d13a329feaf597
Equipo-C <jcbarahona3@espe.edu.ec>
2025-04-29 10:22:05 -0500

    Configura DRF con paginación y filtros

--- pokedex_project/settings.py
+++ pokedex_project/settings.py
@@ -123,3 +123,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Paginación por defecto: 10 elementos por página
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+
+    # Habilitar filtros y búsqueda
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
+}
+
